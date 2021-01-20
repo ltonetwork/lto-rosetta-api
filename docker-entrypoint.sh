125b3bdb238aa4f4ee4e26e92c1e76f6b8413e77
Arnold Daniels <arnold@jasny.net>
2021-01-20 23:30:39 +0100

    docker-entrypoint.sh for lto

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,52 +2,40 @@
 
 # Run nodejs
 cd /usr/src/app/dist/ && forever start server.js
-NETWORKS="mainnet testnet stagenet"
-mkdir -p /var/lib/waves/log
+NETWORKS="mainnet testnet"
+mkdir -p /var/lib/lto/log
 
-if [ ! -f /etc/waves/waves.conf ]; then
-  echo "Custom '/etc/waves/waves.conf' not found. Using a default one for '${WAVES_NETWORK,,}' network." | tee -a /var/log/waves/waves.log
-  if [[ $NETWORKS == *"${WAVES_NETWORK,,}"* ]]; then
-    cp /usr/share/waves/conf/waves-${WAVES_NETWORK}.conf /etc/waves/waves.conf
-    sed -i 's/include "local.conf"//' /etc/waves/waves.conf
-#    for f in /etc/waves/ext/*.conf; do
-#      echo "Adding $f extension config to waves.conf";
-#      echo "include required(\"$f\")" >> /etc/waves/waves.conf
+if [ ! -f "${LTO_CONFIG_FILE}" ]; then
+  echo "Custom '${LTO_CONFIG_FILE}' not found. Using a default one for '${LTO_NETWORK,,}' network." | tee -a /var/log/lto/lto.log
+  if [[ $NETWORKS == *"${LTO_NETWORK,,}"* ]]; then
+    cp /usr/share/lto/conf/lto-${LTO_NETWORK}.conf "${LTO_CONFIG_FILE}"
+    sed -i 's/include "local.conf"//' "${LTO_CONFIG_FILE}"
+#    for f in /etc/lto/ext/*.conf; do
+#      echo "Adding $f extension config to lto.conf";
+#      echo "include required(\"$f\")" >> ${LTO_CONFIG_FILE}
 #    done
-    echo 'include "local.conf"' >> /etc/waves/waves.conf
+    echo 'include "local.conf"' >> "${LTO_CONFIG_FILE}"
   else
-    echo "Network '${WAVES_NETWORK,,}' not found. Exiting."
+    echo "Network '${LTO_NETWORK,,}' not found. Exiting."
     exit 1
   fi
 else
-  echo "Found custom '/etc/waves/waves.conf'. Using it."
+  echo "Found custom '${LTO_CONFIG_FILE}'. Using it."
 fi
 
-if [ "${WAVES_VERSION}" == "latest" ]; then
-  filename=$(find /usr/share/waves/lib -name waves-all* -printf '%f\n')
-  export WAVES_VERSION=$(echo ${filename##*-} | cut -d\. -f1-3)
+if [ "${LTO_VERSION}" == "latest" ]; then
+  filename=$(find /usr/share/lto/lib -name lto-all* -printf '%f\n')
+  export LTO_VERSION=$(echo ${filename##*-} | cut -d\. -f1-3)
 fi
 
-[ -n "${WAVES_WALLET_PASSWORD}" ] && JAVA_OPTS="${JAVA_OPTS} -Dwaves.wallet.password=${WAVES_WALLET_PASSWORD}"
-[ -n "${WAVES_WALLET_SEED}" ] && JAVA_OPTS="${JAVA_OPTS} -Dwaves.wallet.seed=${WAVES_WALLET_SEED}"
+/usr/bin/python3 "/lto-node/starter.py"
 
-JAVA_OPTS="${JAVA_OPTS} -Dwaves.data-directory=/var/lib/waves/data -Dwaves.directory=/var/lib/waves"
+echo "Node is starting..." | tee -a /var/log/lto/lto.log
+echo "LTO_HEAP_SIZE='${LTO_HEAP_SIZE}'" | tee -a /var/log/lto/lto.log
+echo "LTO_LOG_LEVEL='${LTO_LOG_LEVEL}'" | tee -a /var/log/lto/lto.log
+echo "LTO_VERSION='${LTO_VERSION}'" | tee -a /var/log/lto/lto.log
+echo "LTO_NETWORK='${LTO_NETWORK}'" | tee -a /var/log/lto/lto.log
+echo "LTO_WALLET_SEED='${LTO_WALLET_SEED}'" | tee -a /var/log/lto/lto.log
+echo "LTO_WALLET_PASSWORD='${LTO_WALLET_PASSWORD}'" | tee -a /var/log/lto/lto.log
 
-echo "Node is starting..." | tee -a /var/log/waves/waves.log
-echo "WAVES_HEAP_SIZE='${WAVES_HEAP_SIZE}'" | tee -a /var/log/waves/waves.log
-echo "WAVES_LOG_LEVEL='${WAVES_LOG_LEVEL}'" | tee -a /var/log/waves/waves.log
-echo "WAVES_VERSION='${WAVES_VERSION}'" | tee -a /var/log/waves/waves.log
-echo "WAVES_NETWORK='${WAVES_NETWORK}'" | tee -a /var/log/waves/waves.log
-echo "WAVES_WALLET_SEED='${WAVES_WALLET_SEED}'" | tee -a /var/log/waves/waves.log
-echo "WAVES_WALLET_PASSWORD='${WAVES_WALLET_PASSWORD}'" | tee -a /var/log/waves/waves.log
-echo "JAVA_OPTS='${JAVA_OPTS}'" | tee -a /var/log/waves/waves.log
-
-exec java -Dlogback.stdout.level=${WAVES_LOG_LEVEL} \
-  -XX:+ExitOnOutOfMemoryError \
-  -Xmx${WAVES_HEAP_SIZE} \
-  -Dlogback.file.directory=/var/log/waves \
-  -Dconfig.override_with_env_vars=true \
-  ${JAVA_OPTS} \
-  -cp "/usr/share/waves/lib/plugins/*:/usr/share/waves/lib/*" \
-  com.wavesplatform.Application \
-  /etc/waves/waves.conf
+${JAVA_HOME}/bin/java -Dlogback.stdout.level="${LTO_LOG_LEVEL}" "-Xmx${LTO_HEAP_SIZE}" -jar "/lto-node/lto-public-all.jar" $LTO_CONFIG_FILE
